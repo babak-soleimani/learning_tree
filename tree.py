3b0d8db97a80b2cf51455e1fc375ee0b7585727e
Soleimani <bsoleimani@hksinc.com>
2020-10-22 17:51:16 -0400

    some minor changes

--- tree.py
+++ tree.py
@@ -6,24 +6,36 @@ class Node(object):
     def add_child(self, obj):
         self.children.append(obj)
 
-ancestor_input = input("Who are the ancestors? Separate them by space \n") 
-ancestors = [[Node(ancestor) for ancestor in ancestor_input.split(' ')]]
+    # A function to do inorder tree traversal 
+    def printInorder(self): 
+        print (self.data)
+        if self.children != []:
+            for i in self.children:
+                i.printInorder()
 
-while True:
-    ancestors.append([])
-    for ancestor in ancestors[-2]:
-        children_input = input("Who are the children of " + ancestor.data + '\n')
-        for child in children_input.split(' '):
-            ancestor.add_child(child) 
-            new_node = Node(child)
-            ancestors[-1].append(new_node)
+# ancestor_input = input("Who is the ancestor \n") 
+# ancestors = [[Node(ancestor_input)]]
 
-    cont = input("Do you want to add a new generation? Y / N \n")
-    if cont == "Y":    
-        continue
-    if cont == "N":
-        for i in ancestors[0]:
-            print (i.data, i.children)
-        break
-    else:
-        "invalid input. Please answer yes by entering Y and no by entering N"
+# while True:
+#     ancestors.append([])
+#     for ancestor in ancestors[-2]:
+#         children_input = input("Who are the children of " + ancestor.data + '\n')
+#         for child in children_input.split(' '):
+#             new_node = Node(child)
+#             ancestor.add_child(new_node) 
+#             ancestors[-1].append(new_node)
+
+#     while True:
+#         cont = input("Do you want to add a new generation? Y / N \n")
+
+#         if cont == "Y" or "N":    
+#             break
+#         else:
+#             print("invalid input. Please answer yes by entering Y and no by entering N")
+
+#     if cont == "N":
+#         break
+
+# root = ancestors[0][0]
+
+# root.printInorder()
